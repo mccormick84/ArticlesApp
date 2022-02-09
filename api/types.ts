33dcb14519bf89f247648ac69fe6c8086da8e6dc
Kenny <mccormick84@gmail.com>
2022-02-09 18:48:55 +0900

    회원 인증 기능 구현하기(1)
    - 커스텀 Hook 만들기

--- api/types.ts
+++ api/types.ts
@@ -1,5 +1,6 @@
 // 앞으로 사용할 API의 응답 결과에서 사용할 타입을 선언
 // tip : 타입의 수가 다양하다면 종류별로 파일을 나눠서 작성하는 것이 좋을 수 있음
+import {AxiosError} from 'axios';
 
 export interface User {
   id: number;
@@ -36,3 +37,17 @@ export interface AuthResult {
   jwt: string;
   user: User;
 }
+
+type AuthErrorData = {
+  messages: {
+    id: string;
+    message: string;
+  }[];
+}[];
+
+export type AuthError = AxiosError<{
+  statusCode: number;
+  error: string;
+  message: AuthErrorData;
+  data: AuthErrorData;
+}>;
