d9863ecc2f02f78350d28e82f9fe9f6163aa6c83
Kenny <mccormick84@gmail.com>
2022-02-11 18:27:12 +0900

    게시글 작성 기능 구현 4: 게시글 작성 버튼 만들기

--- screens/WriteScreen.tsx
+++ screens/WriteScreen.tsx
@@ -1,16 +1,39 @@
-import React, {useState} from 'react';
+import React, {useCallback, useState, useEffect} from 'react';
 import {
   StyleSheet,
   KeyboardAvoidingView,
   TextInput,
   Platform,
+  Pressable,
 } from 'react-native';
 import {SafeAreaView, useSafeAreaInsets} from 'react-native-safe-area-context';
+import {useNavigation} from '@react-navigation/native';
+import {RootStackNavigationProp} from './types';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 
 export default function WriteScreen() {
   const {top} = useSafeAreaInsets();
   const [title, setTitle] = useState('');
   const [body, setBody] = useState('');
+
+  const navigation = useNavigation<RootStackNavigationProp>();
+  const onSubmit = useCallback(() => {
+    /*TODO: 구현 예정*/
+  }, []);
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerRightContainer: styles.headerRightContainer,
+      headerRight: () => (
+        <Pressable
+          hitSlop={8}
+          onPress={onSubmit}
+          style={({pressed}) => pressed && styles.headerRightPressed}>
+          <MaterialIcons name={'send'} color={'#2196f3'} size={24} />
+        </Pressable>
+      ),
+    });
+  }, [onSubmit, navigation]);
 
   return (
     <SafeAreaView style={styles.block} edges={['bottom']}>
@@ -60,5 +83,11 @@ const styles = StyleSheet.create({
     paddingBottom: 12,
     marginTop: 16,
     flex: 1,
+  },
+  headerRightContainer: {
+    marginRight: 16,
+  },
+  headerRightPressed: {
+    opacity: 0.75,
   },
 });
